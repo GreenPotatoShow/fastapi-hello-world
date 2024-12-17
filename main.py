236d5ce7e89bca85354a23f8bed09c8ebf4ae731
greenpotatoshow <rurururororol@rambler.ru>
2024-12-17 17:13:10 +0700

    3

--- main.py
+++ main.py
@@ -2,39 +2,53 @@ import requests
 import time
 from PIL import Image
 from io import BytesIO
+from fastapi import FastAPI
 
-my_token="7898884050:AAFkWzlGrlJ03pZ9dLUMh7nhZBR5xzucvWY"
-prev_update_id=0
+app = FastAPI()
 
-while True:
-    source = requests.get(f'http://greenpotato.alwaysdata.net/')
-    if source.ok:
-        message=source.json()
-        #print(message["update_id"])
-        if "photo" in message['message']:
-            pic_path = requests.get(f'https://api.telegram.org/bot{my_token}/getFile', params={"file_id": message["message"]['photo'][-1]["file_id"]})
-            pic = requests.get(f'https://api.telegram.org/file/bot{my_token}/{pic_path.json()["result"]["file_path"]}')
-            width=message["message"]["photo"][-1]["width"]
-            height=message["message"]["photo"][-1]["height"]
-            image = Image.open(BytesIO(pic.content))     
-            image=image.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
-            buffered = BytesIO()
-            image.save(buffered, format="PNG")
-            buffered.seek(0)  
 
-            requests.post(f'https://api.telegram.org/bot{my_token}/sendPhoto',
-                        params = {'chat_id': message["message"]["chat"]["id"]},
-                        files={'photo': buffered}
-                        )
-            print(pic)
-        elif "update_id" in message:
-            requests.get(f'https://api.telegram.org/bot{my_token}/sendMessage',
-                        params = {
-                            'chat_id': message["message"]["chat"]["id"],
-                            'text': f'Отправьте фото, оно будет отзеркалено'
-                            }
-                        )
-    time.sleep(0.1)
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/")
+async def mypage():
+    return {"message": "agaga"}
+
+
+# my_token="7898884050:AAFkWzlGrlJ03pZ9dLUMh7nhZBR5xzucvWY"
+# prev_update_id=0
+
+# while True:
+#     source = requests.get(f'http://greenpotato.alwaysdata.net/')
+#     if source.ok:
+#         message=source.json()
+#         #print(message["update_id"])
+#         if "photo" in message['message']:
+#             pic_path = requests.get(f'https://api.telegram.org/bot{my_token}/getFile', params={"file_id": message["message"]['photo'][-1]["file_id"]})
+#             pic = requests.get(f'https://api.telegram.org/file/bot{my_token}/{pic_path.json()["result"]["file_path"]}')
+#             width=message["message"]["photo"][-1]["width"]
+#             height=message["message"]["photo"][-1]["height"]
+#             image = Image.open(BytesIO(pic.content))     
+#             image=image.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
+#             buffered = BytesIO()
+#             image.save(buffered, format="PNG")
+#             buffered.seek(0)  
+
+#             requests.post(f'https://api.telegram.org/bot{my_token}/sendPhoto',
+#                         params = {'chat_id': message["message"]["chat"]["id"]},
+#                         files={'photo': buffered}
+#                         )
+#             print(pic)
+#         elif "update_id" in message:
+#             requests.get(f'https://api.telegram.org/bot{my_token}/sendMessage',
+#                         params = {
+#                             'chat_id': message["message"]["chat"]["id"],
+#                             'text': f'Отправьте фото, оно будет отзеркалено'
+#                             }
+#                         )
+#     time.sleep(0.1)
 
 
 

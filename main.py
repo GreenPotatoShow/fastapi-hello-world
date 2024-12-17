0ce9e490aab721b8fc7b247213d418cb94cdd36e
greenpotatoshow <rurururororol@rambler.ru>
2024-12-17 17:57:58 +0700

    18

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ async def root():
 @app.get("/bot")
 async def bot(request: Request):
     message = await request.json()
-    requests.get(f'https://api.telegram.org/bot{my_token}/sendMessage',
-                    params = {
-                        'chat_id': message["message"]["chat"]["id"],
-                        'text': f'Привет'
-                        }
-                    )
+    # requests.get(f'https://api.telegram.org/bot{my_token}/sendMessage',
+    #                 params = {
+    #                     'chat_id': message["message"]["chat"]["id"],
+    #                     'text': f'Привет'
+    #                     }
+    #                 )
     return {"message": "agaga"}
 
 

5948da574afbe19fba4dc4024704d0affb48e897
greenpotatoshow <rurururororol@rambler.ru>
2024-12-19 17:24:32 +0700

    35

--- main.py
+++ main.py
@@ -15,46 +15,46 @@ async def sum_n_ones(n):
 async def root():
     return {"message": "Hello World"}
 
-# @app.post("/bot/")
-# async def bot(request: Request):
-#     message = await request.json()
-#     chat_id=message["message"]["chat"]["id"]
-#     text=message["message"]["text"]
-#     if re.findall(r'\D', text):
-#         async with httpx.AsyncClient() as client:
-#             await client.get(f'https://api.telegram.org/bot{my_token}/sendMessage',
-#                         params = {
-#                             'chat_id': chat_id,
-#                             'text': f'Введите число n, а я посчитаю сумму 10^n единиц'
-#                             }
-#                         )
-#     else:
-#         n=int(text)
-#         result = await sum_n_ones(n)
-#         async with httpx.AsyncClient() as client:
-#             await client.get(f'https://api.telegram.org/bot{my_token}/sendMessage',
-#                         params = {
-#                             'chat_id': chat_id,
-#                             'text': f'Ответ: {result}'
-#                             }
-#                         )
-#     return message
-
-
 @app.post("/bot/")
 async def bot(request: Request):
     message = await request.json()
-    if "message" in message:
-        chat_id=message["message"]["chat"]["id"]
-        text=message["message"]["text"]
+    chat_id=message["message"]["chat"]["id"]
+    text=message["message"]["text"]
+    if re.findall(r'\D', text):
         async with httpx.AsyncClient() as client:
-                await client.get(f'https://api.telegram.org/bot{my_token}/sendMessage',
-                            params = {
-                                'chat_id': chat_id,
-                                'text': f'Введите число n, а я посчитаю сумму 10^n единиц'
-                                }
-                            )
+            await client.get(f'https://api.telegram.org/bot{my_token}/sendMessage',
+                        params = {
+                            'chat_id': chat_id,
+                            'text': f'Введите число n, а я посчитаю сумму 10^n единиц'
+                            }
+                        )
+    else:
+        n=int(text)
+        result = await sum_n_ones(n)
+        async with httpx.AsyncClient() as client:
+            await client.get(f'https://api.telegram.org/bot{my_token}/sendMessage',
+                        params = {
+                            'chat_id': chat_id,
+                            'text': f'Ответ: {result}'
+                            }
+                        )
     return message
+
+
+# @app.post("/bot/")
+# async def bot(request: Request):
+#     message = await request.json()
+#     if "message" in message:
+#         chat_id=message["message"]["chat"]["id"]
+#         text=message["message"]["text"]
+#         async with httpx.AsyncClient() as client:
+#                 await client.get(f'https://api.telegram.org/bot{my_token}/sendMessage',
+#                             params = {
+#                                 'chat_id': chat_id,
+#                                 'text': f'Введите число n, а я посчитаю сумму 10^n единиц'
+#                                 }
+#                             )
+#     return message
 
 
 

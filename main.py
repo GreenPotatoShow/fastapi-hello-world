02d1b226453e82cb4d9ffd04c8d1027a89080c60
greenpotatoshow <rurururororol@rambler.ru>
2024-12-19 13:13:56 +0700

    21

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ async def root():
 
 @app.get("/bot")
 async def bot(request: Request):
-    if (request.ok):
+    if (request.status_code==200):
         message = await request.json()
     # requests.get(f'https://api.telegram.org/bot{my_token}/sendMessage',
     #                 params = {

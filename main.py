f388533f394dd4204f7c70d87ac27ebbc36b9aeb
greenpotatoshow <rurururororol@rambler.ru>
2024-12-19 14:19:34 +0700

    29

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-import requests
+import re
 import httpx
 from fastapi import FastAPI, Request
 
 app = FastAPI()
 my_token="7898884050:AAFkWzlGrlJ03pZ9dLUMh7nhZBR5xzucvWY"
+
+async def sum_n_ones(n):
+    result=0
+    for i in range(10**n):
+        result+=1
+    return result
 
 @app.get("/")
 async def root():
@@ -14,14 +20,24 @@ async def root():
 async def bot(request: Request):
     message = await request.json()
     chat_id=message["message"]["chat"]["id"]
-    user_name=message["message"]["chat"]["username"]
-    async with httpx.AsyncClient() as client:
+    text=message["message"]["text"]
+    if re.findall(r'\D', text):
         await client.get(f'https://api.telegram.org/bot{my_token}/sendMessage',
                     params = {
                         'chat_id': chat_id,
-                        'text': f'Привет, {user_name}!'
+                        'text': f'Введите число n, а я посчитаю сумму 10^n единиц'
                         }
                     )
+    else:
+        n=int(text)
+        await result = sum_n_ones(n)
+        async with httpx.AsyncClient() as client:
+            await client.get(f'https://api.telegram.org/bot{my_token}/sendMessage',
+                        params = {
+                            'chat_id': chat_id,
+                            'text': f'Ответ: {result}'
+                            }
+                        )
     return message
 
 
